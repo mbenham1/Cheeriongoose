535b3f56585839444cb828c17fa8c5848b708ae9
Benham <Benham@Brets-MacBook-Air.local>
2019-06-02 22:18:30 -0400

    updates

--- public/app.js
+++ public/app.js
@@ -9,6 +9,7 @@
 //     }
 //   });
 
+
 $(document).on("click", "#delete", function () {
 
     var thisID = $(this).attr("data-id");
@@ -31,5 +32,54 @@ $(document).on("click", "#scrape", function () {
     location.reload();
 
 })
+
+$(document).on("click", "#submit", function () {
+    // Grab the id associated with the article from the submit button
+    var thisID = $(this).attr("data-id");
+
+    // Run a POST request to change the note, using what's entered in the inputs
+    $.ajax({
+        method: "POST",
+        url: "/articles/" + thisID,
+        data: {
+            body: $("#add-comment").val()
+        }
+    })
+        // With that done
+        .then(function (data) {
+            // Log the response
+            console.log(data);
+            // Empty the notes section
+            $("#notes").empty();
+        });
+
+    // Also, remove the values entered in the input and textarea for note entry
+    $("#add-comment").val("");
+});
+
+// $(document).on("click", "#comment", function() {
+//     $("#comment-modal").modal();
+// });
+
+// $(document).on("click", "#submit", addComment);
+
+// function addComment(event) {
+// 	event.preventDefault();
+
+// 	var comment = $("#add-comment").val();
+//     var thisID = $(this).attr("data-id");
+
+// 	$.ajax({
+// 		method: "POST",
+// 		url: "/articles/" + thisID,
+// 		data: {
+// 			body: comment
+// 		}
+// 	})
+// 		.then(function(data) {
+//             $("#add-comment").val("");
+// 		});
+// }
+
 
 
